9499fec1e73d03c43fbf1ebba4beca90f92b1057
YBS <ybs100411@gmail.com>
2021-06-13 16:11:28 +0900

    [labiss] Develop back_test logic

--- main.py
+++ main.py
@@ -8,7 +8,6 @@ from statsmodels.tsa.stattools import coint, adfuller
 from datetime import datetime
 pd.core.common.is_list_like = pd.api.types.is_list_like
 import schedule as sch
-import datetime
 import telegram
 
 TEST_MODE = 3
@@ -17,6 +16,15 @@ TEST_MODE = 3
 # 1 : logic_test + telegram test
 # 2 : real test (매수 매도 진행)
 # 3 : back test
+
+position = 0
+buy_long_price = 0
+buy_short_price = 0
+buy_long_symbol = 0
+buy_short_symbol = 0
+buy_long_num = 0
+buy_short_num = 0
+money = 100
 
 if (TEST_MODE > 0):
     # telegram bot labiss_Bot
@@ -36,7 +44,7 @@ def stationarity_test(X, cutoff=0.01):
     else:
         print('p-value = ' + str(pvalue) + ' The series ' + X.name +' is likely non-stationary.')
 
-def show_plot(spread_df, title):
+def show_plot(spread_df, mean, std_plus, std_minus, title):
     print('show plot')
     # spread_df의 평균과 그래프
     ax = plt.gca()
@@ -44,9 +52,9 @@ def show_plot(spread_df, title):
     plt.xlabel('5m candle')
     plt.ylabel(title)
     spread_df.plot(kind='line', x='datetime', y='spread', ax=ax)
-    spread_df.plot(kind='line', x='datetime', y='mean', ax=ax, color='black', linestyle='--')
-    spread_df.plot(kind='line', x='datetime', y='std_plus', ax=ax, color='green', linestyle='--')
-    spread_df.plot(kind='line', x='datetime', y='std_minus', ax=ax, color='red', linestyle='--')
+    plt.axhline(y=mean, color='black', linestyle='--')
+    plt.axhline(y=std_plus, color='green', linestyle='--')
+    plt.axhline(y=std_minus, color='red', linestyle='--')
     # img 저장
     # file_path 최적화 필요
     filename = "".join(i for i in title if i not in "\/:*?<>|")
@@ -81,7 +89,7 @@ def find_coint_coefficient(pair_df, pair1, pair2):
         else:
             pvalue = pvalue
     print("pvalue min = ", pvalue, "CC = ", min_cc)
-    return stable_spread_df['spread'], min_cc
+    return stable_spread_df['spread'], min_cc, pvalue
 
 # compute log spread
 def compute_log_spread(pair_df, pair1, pair2, score):
@@ -90,23 +98,23 @@ def compute_log_spread(pair_df, pair1, pair2, score):
     spread_df['datetime'] = pair_df['datetime']
     spread_df[pair1] = pair_df[pair1]
     spread_df[pair2] = pair_df[pair2]
-    spread_df['spread'], CC = find_coint_coefficient(pair_df, pair1, pair2)
+    spread_df['spread'], CC, pvalue = find_coint_coefficient(pair_df, pair1, pair2)
     # moving average window
     #TODO 뒤에서 몇개 가져올지
-    window_ = 300
-    spread_df['mean'] = spread_df['spread'].rolling(window_).mean()
-    spread_df['std'] = spread_df['spread'].rolling(window_).std()
-    spread_df['std_plus'] = spread_df['mean'] + spread_df['std']
-    spread_df['std_minus'] = spread_df['mean'] - spread_df['std']
+    mean = spread_df['spread'].mean()
+    std = spread_df['spread'].std()
+    std_plus = mean+std
+    std_minus = mean-std
     title = str(pair_df['datetime'][0]) + '_' + str(pair1) + '-' + str(pair2) + ' SPREAD'
-    show_plot(spread_df, title)
+    show_plot(pair_df, mean, std_plus, std_minus, title)
 
 # find cointegrated_pairs for 25 coins in USDT market
 def find_cointegrated_pairs(data):
     n = data.shape[1]
     print('n', n)
     score_matrix = np.zeros((n, n))
-    pvalue_matrix = np.ones((n, n))
+    # pvalue_matrix = np.ones((n, n))
+    pvalue_dict = {}
     keys = data.keys()
     pairs = []
     for i in range(n):
@@ -117,11 +125,12 @@ def find_cointegrated_pairs(data):
             score = result[0]
             pvalue = result[1]
             score_matrix[i, j] = score
-            pvalue_matrix[i, j] = pvalue
+            # pvalue_matrix[i, j] = pvalue
             # 0.05 면 충분히 작은가?
             if pvalue < 0.05:
                 pairs.append((keys[i], keys[j]))
-    return score_matrix, pvalue_matrix, pairs
+                pvalue_dict[(keys[i], keys[j])] = pvalue
+    return score_matrix, pvalue_dict, pairs
 
 # market data 긁어오기
 #TODO 변동성이 작을 경우 너무 많은 페어를 잡음
@@ -158,76 +167,113 @@ def market_search(market_search_space):
         compute_log_spread(pair_df, pair[0] ,pair[1], scores[i][j])
     print("Find pair : " ,len(pairs))
 
+def select_action(pair_df, mean, std, std_plus, std_minus, CC, pvalue):
+    # action
+    # 0 == 아무것도, 1 == 매수, 2 == 수익실현, 3 == 손절,
+    curr_value = pair_df['spread'].iloc[-1]
+    past_value = pair_df['spread'].iloc[-2]
+    #print(curr_value)
+    print('==================== select_action ====================')
+    global position, buy_long_price, buy_short_price, buy_long_symbol, buy_short_symbol, buy_long_num, buy_short_num, money
+    print('position, buy_long_price, buy_short_price, buy_long_symbol, buy_short_symbol, buy_long_num, buy_short_num, money')
+    print(position, buy_long_price, buy_short_price, buy_long_symbol, buy_short_symbol, buy_long_num, buy_short_num, money)
+    # 수익실현
+    if position == 1 and ((mean - past_value) * (mean - curr_value) <= 0):
+        print('+++++++++++++good sell +++++++++++++')
+        money += (buy_long_price - pair_df[buy_long_symbol].iloc[-1]) * buy_long_num \
+                 - (buy_short_price - pair_df[buy_short_symbol].iloc[-1]) * buy_short_num
+        position = 0
+    # 손절 (일단 2 sigma) Warning Value
+    if position == 1 and ((pvalue > 0.05) or (curr_value >= mean+2*std or curr_value <= mean-2*std)):
+        print('+++++++++++++10 sell+++++++++++++')
+        money += (buy_long_price-pair_df[buy_long_symbol].iloc[-1])*buy_long_num \
+                - (buy_short_price-pair_df[buy_short_symbol].iloc[-1])*buy_short_num
+        position = 0
+    # 매수
+    if position == 0 and (curr_value >= std_plus) and ((pvalue < 0.05) and (curr_value <= mean+2*std and curr_value >= mean-2*std)):
+        print('+++++++++++++buy+++++++++++++')
+        buy_long_symbol = pair_df.columns.values[2]
+        buy_short_symbol = pair_df.columns.values[1]
+        buy_long_price = pair_df[buy_long_symbol].iloc[-1]
+        buy_short_price = pair_df[buy_short_symbol].iloc[-1]
+        buy_long_num = money / 2.0 / buy_long_price
+        buy_short_num = money / 2.0 / buy_short_price
+        position = 1
+    elif position == 0 and (curr_value <= std_minus) and ((pvalue < 0.05) and (curr_value <= mean+2*std and curr_value >= mean-2*std)):
+        print('+++++++++++++buy+++++++++++++')
+        buy_long_symbol = pair_df.columns.values[1]
+        buy_short_symbol = pair_df.columns.values[2]
+        buy_long_price = pair_df[buy_long_symbol].iloc[-1]
+        buy_short_price = pair_df[buy_short_symbol].iloc[-1]
+        buy_long_num = money / 2.0 / buy_long_price
+        buy_short_num = money / 2.0 / buy_short_price
+        position = 1
+
+
+
 # leverage = 배율
 # candle_count = 5분봉 몇개
 # money
-def back_test(money, leverage, candle_count, since=None):
+def back_test(market_search_space, money, leverage, candle_count, window, since=None):
     print('********* [LABISS Backtest] *********')
-    print('현재 잔고 : ', money, 'USDT, leverage : x', leverage, ' Number of Candle : ', candle_count)
+    print('현재 잔고 : ', money, 'USDT, leverage : x', leverage, ' Number of Candle : ', candle_count, ' window : ', window)
+    pair_df_raw_ = pd.DataFrame()
+    pair_df_raw = pd.DataFrame()
     pair_df = pd.DataFrame()
-    pair1 = 'ETH/USDT'
-    pair2 = 'AAVE/USDT'
-    for market_key in [pair1, pair2]:
-        ohlcv = binance.fetch_ohlcv(symbol=market_key, timeframe='5m', limit=candle_count)
-        df = pd.DataFrame(ohlcv, columns=['datetime', 'open', 'high', 'low', 'close', 'volume'])
-        df['datetime'] = pd.to_datetime(df['datetime'], unit='ms')
-        pair_df['datetime'] = df['datetime']
-        pair_df[market_key] = df['close']
-    pair_df['spread'], CC = find_coint_coefficient(pair_df, pair1, pair2)
+    # pair1 = 'ETH/USDT'
+    # pair2 = 'AAVE/USDT'
+    # for market_key in [pair1, pair2]:
+    #     ohlcv = binance.fetch_ohlcv(symbol=market_key, timeframe='5m', limit=candle_count)
+    #     df = pd.DataFrame(ohlcv, columns=['datetime', 'open', 'high', 'low', 'close', 'volume'])
+    #     df['datetime'] = pd.to_datetime(df['datetime'], unit='ms')
+    #     pair_df_raw['datetime'] = df['datetime']
+    #     pair_df_raw[market_key] = df['close']
+    for market_key in market_search_space:
+            ohlcv = binance.fetch_ohlcv(symbol=market_key, timeframe='5m', limit=candle_count)
+            df = pd.DataFrame(ohlcv, columns=['datetime', 'open', 'high', 'low', 'close', 'volume'])
+            df['datetime'] = pd.to_datetime(df['datetime'], unit='ms')
+            pair_df_raw_['datetime'] = df['datetime']
+            pair_df_raw_[market_key] = df['close']
 
-    pair_df['mean'] = pair_df['spread'].mean()
-    pair_df['std'] = pair_df['spread'].std()
-    pair_df['std_plus'] = pair_df['mean'] + pair_df['std']
-    pair_df['std_minus'] = pair_df['mean'] - pair_df['std']
-    title = str(pair_df['datetime'][0]) + '_' + str(pair1) + '-' + str(pair2) + ' SPREAD'
+    # pair 찾기
+    pair_df_ = pair_df_raw_.iloc[0:window]
+    scores, pvalues, pairs = find_cointegrated_pairs(pair_df_.iloc[:, 1:26])
+    pair = min(pvalues.keys(), key=lambda k: pvalues[k])
+    pair1 = pair[0]
+    pair2 = pair[1]
+    print('min pvalue pair : ', pair[0],'-',pair[1])
 
-    mean = pair_df['spread'].mean()
-    std = pair_df['spread'].std()
-    std_plus = mean+std
-    std_minus = mean-std
-    eat_count = 0
-    mean_index_array=[]
-    std_index_array=[]
-    for i in range(candle_count-1):
-        a = pair_df['spread'][i]
-        b = pair_df['spread'][i+1]
-        if ((mean-a)*(mean-b)<=0):
-            mean_index_array.append(i+1)
-        if (((std_plus-a)*(std_plus-b)<=0) or ((std_minus-a)*(std_minus-b)<=0)):
-            std_index_array.append(i)
-    print('length of mean_index_array : ', len(mean_index_array))
-    print('mean_index_array', mean_index_array)
-    print('length of std_index_array : ', len(std_index_array))
-    print('std_index_array', std_index_array)
+    pair_df_raw['datetime'] = pair_df_raw_['datetime']
+    pair_df_raw[pair1] = pair_df_raw_[pair1]
+    pair_df_raw[pair2] = pair_df_raw_[pair2]
+    print(pair_df_raw)
 
-    for i, mean_ in enumerate(mean_index_array[:-1]):
-        for std_ in std_index_array:
-            print('test std_ : ',std_, ' mean[i] : ',mean_index_array[i], ' mean[i+1] : ', mean_index_array[i+1])
-            if (std_ >= mean_index_array[i] and std_ <= mean_index_array[i + 1]):
-                eat_count += 1
-                print('eat_count : ', eat_count)
-                break
+    init_money = money
+    for i in range(0, candle_count-window):
+        pair_df = pair_df_raw.iloc[i:i+window]
+        # scores, pvalues, pairs = find_cointegrated_pairs(pair_df_.iloc[:, 1:26])
+        # print(min(pvalues.keys(), key=lambda k: pvalues[k]))
+        # pair = min(pvalues.keys(), key=lambda k: pvalues[k])
+        # pair1 = pair[0]
+        # pair2 = pair[1]
+        # print('min pvalue pair : ', pair[0],'-',pair[1])
+        # pair_df = pd.DataFrame()
+        # pair_df['datetime'] = pair_df_['datetime']
+        # pair_df[pair1] = pair_df_[pair1]
+        # pair_df[pair2] = pair_df_[pair2]
+        # print(pair_df)
+        pair_df['spread'], CC , pvalue = find_coint_coefficient(pair_df, pair1, pair2)
+        mean = pair_df['spread'].mean()
+        std = pair_df['spread'].std()
+        std_plus = mean+std
+        std_minus = mean-std
+        title = "test"
+        # print(pair_df)
+        select_action(pair_df, mean, std, std_plus, std_minus ,CC, pvalue)
+        # show_plot(pair_df, mean, std_plus, std_minus, title)
+        print('Show me the money : ', money)
 
-    print('eat_count : ', eat_count)
-
-    A1 = pair_df[pair1][mean_index_array[0]]
-    B1 = pair_df[pair2][mean_index_array[0]]
-    A2 = pair_df[pair1][std_index_array[0]]
-    B2 = pair_df[pair2][std_index_array[0]]
-    print(A1, B1, A2, B2)
-    money1 = money2 = money/2
-    print(money1, money2)
-    profit = (A1/A2 + B2/B1)*eat_count
-
-    print('before : ', money, ' after : ', money+profit , ' ratio : ', (money+profit)/(money)*100, '%')
-
-
-    # 수익률
-    # pair_df['ror'] = np.where(pair_df['spread'] > pair
-    #
-    # )
-    show_plot(pair_df, title)
-    print(pair_df)
+    print('Ratio', money/init_money*100)
 
 if __name__ == '__main__':
     binance = ccxt.binance()
@@ -241,7 +287,8 @@ if __name__ == '__main__':
     if (TEST_MODE != 3):
         market_search(market_search_space)
     else:
-        back_test(100, 5, 100)
+        # money, leverage, candle_count, window
+        back_test(market_search_space, money, 5, 1000, 800)
     # sch.every(5).minutes.do(market_search,market_search_space)
 
     # while True:
